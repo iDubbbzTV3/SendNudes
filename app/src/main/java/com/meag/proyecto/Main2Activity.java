4d90e935798e610fb2575e0644e2a056e4cbd22e
00017316 <00017316@uca.edu.sv>
2018-04-05 12:29:35 -0600

    Post

--- app/src/main/java/com/meag/proyecto/Main2Activity.java
+++ app/src/main/java/com/meag/proyecto/Main2Activity.java
@@ -1,13 +1,48 @@
 package com.meag.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    TextView text;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        text = findViewById(R.id.textView2);
+        but = findViewById(R.id.button);
+
+        Intent callingIntent = getIntent();
+        String intentAction = callingIntent.getAction();
+        String intentType = callingIntent.getType();
+
+        if (Intent.ACTION_SEND.equals(intentAction) && intentType != null){
+            if (intentType.equals("text/plain")) {
+                handleReceivedText(callingIntent);
+            }
+        }
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fstScreenIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(fstScreenIntent);
+            }
+        });
+    }
+
+    public void handleReceivedText(Intent i){
+        String intentText = i.getStringExtra(Intent.EXTRA_TEXT);
+
+        if(text != null){
+            text.setText(intentText);
+        }
     }
 }
